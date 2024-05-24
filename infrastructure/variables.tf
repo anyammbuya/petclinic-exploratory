66bd69a384f13ff8873a2b72d7c55cb5151f5688
anyammbuya <145148751+anyammbuya@users.noreply.github.com>
2024-05-23 22:43:39 -0700

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,12 +1,12 @@
 variable "region" {
   description =  "Identifies AWS region to use"
-  default = "us-west-1"
+  default = "us-east-2"
 }
 
 
 variable "vpc_id" {
   description =  "VPC to use for testing. Default is AWS Default VPC"
-  default = "vpc-442aaf21"
+  default = "vpc-0428fe6a97b0ea753"
 }
 
 
